f2ba71bcb77bc11e8142c7f9c0efbe6855d7d40a
Osho <arnav.saraf777@gmail.com>
2017-10-06 11:11:07 +0530

    Update template.cpp
    Template updated

--- OOP/template.cpp
+++ OOP/template.cpp
@@ -1,13 +1,49 @@
 #include<iostream>
+#include<iomanip>	//setw() will be used to align the output 
 
 using namespace std;
 
-template<class T>
-void tt(){
-	T a="string";
-	cout<<a;
+template<class sort>// Here sort is a generic datatype which will replace the datatype of the array passed
+void selectionSort(sort* arr, int size){
+	sort temp;
+	for(int i=0; i<size; i++){ // Algorithm for selection sort
+		for(int j=i; j<size; j++){
+			if(arr[j]<arr[i]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}                     
+	}
 }
 
+template<class sort>
+void displayArray(sort* arr, int size){
+	for(int i=0; i<size; i++){
+		cout<<setw(5)<<arr[i]<<" ";
+	}	
+	cout<<endl;
+}
+
+
 int main(){
-	tt();
+	int array_1[] = {2,3,4,5,6,1,34};
+	float array_2[] = {2.2,3,4,5,6,1,34};
+	char array_3[] = "arnavsa";
+	string array_4 = "rafarna";
+
+	selectionSort(array_1,7);
+	displayArray(array_1,7);
+	
+	selectionSort(array_2,7);
+	displayArray(array_2,7);
+	
+	selectionSort(array_3,7);
+	displayArray(array_3,7);
+	
+	selectionSort(&array_4[0],7);
+	displayArray(&array_4[0],7);
+	
+	 
+	return 0;
 }
